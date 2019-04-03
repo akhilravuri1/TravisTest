b32d517a8da5e1408ae9b46bbbe2da7a18e1a695
akhilravuri1 <rakhil@rocketsoftware.com>
2019-04-03 16:56:41 +0530

    new test case

--- testing.go
+++ testing.go
@@ -1,15 +1,16 @@
 package main
 
 import (
-    _ "github.com/ibmdb/go_ibm_db"
-    "database/sql"
-    "fmt"
+	"database/sql"
+	"fmt"
+	_ "github.com/ibmdb/go_ibm_db"
 )
 
-func Create_Con(con string) *sql.DB{
- db, err:=sql.Open("go_ibm_db", con)
-    if err != nil{
-        
+func Create_Con(con string) *sql.DB {
+	fmt.Println("create conn func")
+	db, err := sql.Open("go_ibm_db", con)
+	if err != nil {
+
 		fmt.Println(err)
 		return nil
 	}
@@ -18,84 +19,32 @@ func Create_Con(con string) *sql.DB{
 
 //creating a table
 
-func create(db *sql.DB) error{
-    _,err:=db.Exec("DROP table SAMPLE")
-	if(err!=nil){
-    _,err:=db.Exec("create table SAMPLE(ID varchar(20),NAME varchar(20),LOCATION varchar(20),POSITION varchar(20))")
-    if err != nil{
-        return err
-    }
+func create(db *sql.DB) error {
+	_, err := db.Exec("DROP table SAMPLE")
+	fmt.Println("This is table create")
+	if err != nil {
+		_, err := db.Exec("create table SAMPLE(ID varchar(20),NAME varchar(20),LOCATION varchar(20),POSITION varchar(20))")
+		if err != nil {
+			return err
+		}
 	} else {
-    _,err:=db.Exec("create table SAMPLE(ID varchar(20),NAME varchar(20),LOCATION varchar(20),POSITION varchar(20))")
-    if err != nil{
-        return err
-    }
+		_, err := db.Exec("create table SAMPLE(ID varchar(20),NAME varchar(20),LOCATION varchar(20),POSITION varchar(20))")
+		if err != nil {
+			return err
+		}
 	}
 	fmt.Println("TABLE CREATED")
-    return nil
+	return nil
 }
 
-//inserting row
-
-func insert(db *sql.DB) error{
-    st, err:=db.Prepare("Insert into SAMPLE(ID,NAME,LOCATION,POSITION) values('3242','mike','hyd','manager')")
-    if err != nil{
-        return err
-    }
-    st.Query()
-    return nil
-}
-
-//this api selects the data from the table and prints it
-
-func display(db *sql.DB) error{
-    st, err:=db.Prepare("select * from SAMPLE")
-    if err !=nil{
-        return err
-    }
-    err=execquery(st)
-    if err!=nil{
-        return err
-    }
-    return nil
-}
-
-
-func execquery(st *sql.Stmt) error{
-    rows,err :=st.Query()
-    if err != nil{
-        return err
-    }
-	cols, _ := rows.Columns()
-    fmt.Printf("%s    %s   %s    %s\n",cols[0],cols[1],cols[2],cols[3])
-    fmt.Println("-------------------------------------")
-    defer rows.Close()
-    for rows.Next(){
-        var t,x,m,n string
-        err = rows.Scan(&t,&x,&m,&n)
-        if err != nil{
-            return err
-        }
-        fmt.Printf("%v  %v   %v         %v\n",t,x,m,n)
-    }
-    return nil
-}
-
-func main(){
-    con:="HOSTNAME=10.45.6.33;DATABASE=go;PORT=50000;UID=rakhil;PWD=Spranu134#"
+func main() {
+	fmt.Println("This is main")
+	con := "HOSTNAME=10.45.6.33;DATABASE=go;PORT=50000;UID=rakhil;PWD=Spranu134#"
 	type Db *sql.DB
 	var re Db
-	re=Create_Con(con)
-    err:=create(re)
-	if err != nil{
-        fmt.Println(err)
-    }
-    err=insert(re)
-    if err != nil{
-        fmt.Println(err)
-    }
-    err=display(re)
-    if err != nil{
-        fmt.Println(err)
-    }
+	re = Create_Con(con)
+	err := create(re)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
