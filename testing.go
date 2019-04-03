d4141e374c56d9a97f882afec48a393d8f801c38
akhilravuri1 <rakhil@rocketsoftware.com>
2019-04-03 12:59:22 +0530

    initial commit

---
+++ testing.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func akhil(){
+	fmt.Println("this is akhil")
+}
+
+func nikhil(){
+	fmt.Println("this is nikhil")
+}
+
+func main(){
+	fmt.Println("this is main")
+	akhil()
+	nikhil()
+}
