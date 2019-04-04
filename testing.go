b29a302ac356134ee1454a268c9650a3401da618
akhilravuri1 <rakhil@rocketsoftware.com>
2019-04-04 10:56:49 +0530

    connection string

--- testing.go
+++ testing.go
@@ -1,16 +1,15 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
-	_ "github.com/ibmdb/go_ibm_db"
+    _ "github.com/ibmdb/go_ibm_db"
+    "database/sql"
+    "fmt"
 )
 
-func Create_Con(con string) *sql.DB {
-	fmt.Println("create conn func")
-	db, err := sql.Open("go_ibm_db", con)
-	if err != nil {
-
+func Create_Con(con string) *sql.DB{
+ db, err:=sql.Open("go_ibm_db", con)
+    if err != nil{
+        
 		fmt.Println(err)
 		return nil
 	}
@@ -19,34 +18,86 @@ func Create_Con(con string) *sql.DB {
 
 //creating a table
 
-func create(db *sql.DB) error {
-	_, err := db.Exec("DROP table SAMPLE")
-	fmt.Println(err)
-	fmt.Println("This is table create")
-	if err != nil {
-		_, err := db.Exec("create table SAMPLE(ID varchar(20),NAME varchar(20),LOCATION varchar(20),POSITION varchar(20))")
-		if err != nil {
-		    fmt.Println("this is inside error") 
-			return err
-		}
+func create(db *sql.DB) error{
+    _,err:=db.Exec("DROP table SAMPLE")
+	if(err!=nil){
+    _,err:=db.Exec("create table SAMPLE(ID int,NAME varchar(20),LOCATION float,POSITION varchar(20))")
+    if err != nil{
+        return err
+    }
 	} else {
-		_, err := db.Exec("create table SAMPLE(ID varchar(20),NAME varchar(20),LOCATION varchar(20),POSITION varchar(20))")
-		if err != nil {
-			return err
-		}
+    _,err:=db.Exec("create table SAMPLE(ID int,NAME varchar(20),LOCATION float,POSITION varchar(20))")
+    if err != nil{
+        return err
+    }
 	}
 	fmt.Println("TABLE CREATED")
-	return nil
+    return nil
 }
 
-func main() {
-	fmt.Println("This is main")
-	con := "HOSTNAME=dashdb-entry-yp-lon02-01.services.eu-gb.bluemix.net;DATABASE=BLUDB;PORT=50000;UID=dash13365;PWD=_w0KbssHT3_I"
+//inserting row
+
+func insert(db *sql.DB) error{
+    st, err:=db.Prepare("Insert into SAMPLE(ID,NAME,LOCATION,POSITION) values(3242,'mike',123.24,'manager')")
+    if err != nil{
+        return err
+    }
+    st.Query()
+    return nil
+}
+
+//this api selects the data from the table and prints it
+
+func display(db *sql.DB) error{
+    st, err:=db.Prepare("select * from SAMPLE")
+    if err !=nil{
+        return err
+    }
+    err=execquery(st)
+    if err!=nil{
+        return err
+    }
+    return nil
+}
+
+
+func execquery(st *sql.Stmt) error{
+    rows,err :=st.Query()
+    if err != nil{
+        return err
+    }
+	cols, _ := rows.Columns()
+    fmt.Printf("%s    %s   %s    %s\n",cols[0],cols[1],cols[2],cols[3])
+    fmt.Println("-------------------------------------")
+    defer rows.Close()
+    for rows.Next(){
+		var t int
+		var m float64
+        var x,n string
+        err = rows.Scan(&t,&x,&m,&n)
+        if err != nil{
+            return err
+        }
+        fmt.Printf("%v  %v   %v         %v\n",t,x,m,n)
+    }
+    return nil
+}
+
+func main(){
+    con := "HOSTNAME=dashdb-entry-yp-lon02-01.services.eu-gb.bluemix.net;DATABASE=BLUDB;PORT=50000;UID=dash13365;PWD=_w0KbssHT3_I"
 	type Db *sql.DB
 	var re Db
-	re = Create_Con(con)
-	err := create(re)
-	if err != nil {
-		fmt.Println(err)
-	}
+	re=Create_Con(con)
+    err:=create(re)
+	if err != nil{
+        fmt.Println(err)
+    }
+    err=insert(re)
+    if err != nil{
+        fmt.Println(err)
+    }
+    err=display(re)
+    if err != nil{
+        fmt.Println(err)
+    }
 }
